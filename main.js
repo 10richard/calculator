bbb9f9e605b366e588b1f59cdd94ecafa6de9706
Richard <rten04@icloud.com>
2022-10-24 23:58:40 -0400

    Add evaluating functions and new variables
    Added lstOperator(array), total(int) and evaluate(boolean). Might use evaluate
    in future.
    
    When operator is clicked on calculator, it will be appended to lstOperator.
    If lstOperator length == 1, the next nums clicked will be appended
    to secondOperand. Otherwise, the nums will be appended to firstOperand.
    
    If lstOperator > 1, then one of the evaluating functions will be called on.
    (will check first item of lstOperator) ie/ ['+', '-'] --> add()
    
    When equation is executed, the operator will be removed with .pop function
    from lstOperator.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let operator = ""
-let firstOperand = ""
-let secondOperand = ""
+let lstOperator = [];
+let firstOperand = "";
+let secondOperand = "";
+let total = 0;
+let evaluate = false;
 
 
 const numberBtns = document.querySelectorAll('.btn.number');
@@ -8,33 +10,71 @@ const operatorBtns = document.querySelectorAll('.btn.operator');
 
 
 numberBtns.forEach(btn => btn.addEventListener('click', () => {
-    setNum(btn.textContent);
+    appendNum(btn.textContent);
 }));
 
 operatorBtns.forEach(btn => btn.addEventListener('click', () => {
-    setOperator(btn.textContent)
+    appendOperator(btn.textContent)
 }));
 
-function setNum(num) {
-    if (operator != "") {
-        secondOperand += parseInt(num);
-        console.log(secondOperand)
+function appendNum(num) {
+    if (lstOperator.length == 1) {
+        secondOperand += num;
+        console.log(secondOperand);
     } else {
-        firstOperand += parseInt(num);
-        console.log(firstOperand)
-    }
-};
-
-function setOperator(check) {
-    if (operator == "") {
-        operator += check;
-        console.log(operator);
-    } else {
-        operator += check;
-        console.log(operator)
+        firstOperand += num;
+        console.log(firstOperand);
     }
 }
 
-function checkOperator(check) {
+function appendOperator(operator) {
+    lstOperator.push(operator);
+    console.log(lstOperator);
+    if (lstOperator.length > 1) {
+        checkOperator()
+    }
+}
 
+function checkOperator() {
+    firstOperator = lstOperator.slice(0, 1);
+    if (firstOperator == "+") {
+        add()
+    } else if (firstOperator == '-') {
+        subtract()
+    } else if (firstOperator == 'x') {
+        multiply()
+    } else if (firstOperator == '÷') {
+        divide()
+    } else {
+        console.log('error: operator is wrong');
+        console.log(lstOperator);
+    }
+}
+
+function add() {
+    total += (parseInt(firstOperand) + parseInt(secondOperand));
+    lstOperator.pop('+');
+    console.log(lstOperator);
+    console.log(total);
+}
+
+function subtract() {
+    total += (parseInt(firstOperand) - parseInt(secondOperand));
+    lstOperator.pop('-');
+    console.log(lstOperator);
+    console.log(total); 
+}
+
+function divide() {
+    total += (parseInt(firstOperand) / parseInt(secondOperand));
+    lstOperator.pop('÷');
+    console.log(lstOperator);
+    console.log(total);
+}
+
+function multiply() {
+    total += (parseInt(firstOperand) * parseInt(secondOperand));
+    lstOperator.pop('x');
+    console.log(lstOperator);
+    console.log(total);
 }
